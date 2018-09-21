e478c3eb720c7e7aaf1a7dd9f20a6b544aac5e15
Collins Juma <collinsjumaz42@gmail.com>
2018-09-21 17:50:54 +0300

    minified js

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,25 @@ gulp.task('imageMin', () =>
 		.pipe(imagemin())
 		.pipe(gulp.dest('dist/images'))
 );
+
+// Minify JS
+gulp.task('minify', function(){
+  gulp.src('src/js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+});
+
+// Scripts
+gulp.task('scripts', function(){
+  gulp.src('src/js/*.js')
+      .pipe(concat('main.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', [ 'imageMin', 'scripts']);
+
+gulp.task('watch', function(){
+  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('src/images/*', ['imageMin']);
+});
